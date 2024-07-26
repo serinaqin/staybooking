21374b8e10bc3ceeb87ea2f7b1f64a41b7b0b4be
serinaqin <qin.zhen@northeastern.edu>
2024-07-25 18:53:44 -0700

    feat: set up gcloud and storage service

--- src/main/java/com/serina/staybooking/AppConfig.java
+++ src/main/java/com/serina/staybooking/AppConfig.java
@@ -1,10 +1,16 @@
 package com.serina.staybooking;
 
+import com.google.auth.Credentials;
+import com.google.auth.oauth2.ServiceAccountCredentials;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.io.IOException;
 
 
 @Configuration
@@ -23,6 +29,12 @@ public class AppConfig {
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         return http.build();
+    }
+
+    @Bean
+    public Storage storage() throws IOException {
+        Credentials credentials = ServiceAccountCredentials.fromStream(getClass().getClassLoader().getResourceAsStream("credentials.json"));
+        return StorageOptions.newBuilder().setCredentials(credentials).build().getService();
     }
 }
 
